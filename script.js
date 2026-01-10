832dfbee07cbc0880e8c145ccce91147d70f0e41
Samad <Samad.o@outlook.com>
2026-01-10 17:37:39 -0500

    calc evaluation complete. no letter inputs required.

---
+++ script.js
@@ -0,0 +1,25 @@
+const keys = document.querySelectorAll(".calculator__key");
+const input = document.querySelector(".input");
+const output = document.querySelector("#equations");
+
+input.addEventListener("click", (e) => {
+  function addContent() {
+    if (!e.target.matches("button")) return;
+    inputValue = e.target.textContent;
+
+    if (inputValue === "delete") {
+      output.value = output.value.slice(0, -1);
+      return;
+    }
+    if (inputValue === "="){
+      output.value = Function('return ' + output.value)();
+      return;
+    }
+
+    
+
+    output.value += inputValue;
+  }
+
+  addContent();
+});
